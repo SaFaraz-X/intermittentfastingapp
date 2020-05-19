7972eaac1ea6eef92cf0083c1004e68375d17444
matthewmckeon <52587397+matthewmckeon@users.noreply.github.com>
2020-05-19 14:25:33 -0400

    Add files via upload

---
+++ About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { black } from 'color-name'
+
+const about = () => {
+    return(
+        <div className="About">
+        <h1>What is SmartFast?</h1>
+        <p>SmartFast is a platform that allows users to take control of thier intermittent fasting. We help you track your fasting duration, monitor your fasting history, and keep you motivated. </p>
+        <button>Start Fasting!</button>
+      </div>
+    )
+}
+
+
+export default about;
---
+++ App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+import About from './About.js'
+
+class App extends Component {
+  render() {
+    return (
+      <About />
+    );
+  }
+}
+
+
+export default App;
