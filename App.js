98688089229d879f1bdebda3c791f6bdba6803ad
matthewmckeon <52587397+matthewmckeon@users.noreply.github.com>
2020-05-20 14:32:44 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Nav from './Nav.js'
-import About from './About.js'
+import Home from './Home.js'
 import history from './history.js'
 
 
@@ -9,10 +9,10 @@ class App extends Component {
 
   render(){
     return(
-<div>
-      <Nav/>
-      <About />
-     <historyChart/>
+      <div>
+        <Nav/>
+        <Home />
+        <historyChart/>
       </div>
     );
   }
