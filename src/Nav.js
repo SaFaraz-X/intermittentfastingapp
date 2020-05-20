9f81bd2bdfdec085f6520bebc9e813a5b189d053
matthewmckeon <52587397+matthewmckeon@users.noreply.github.com>
2020-05-20 14:37:59 -0400

    Update Nav.js

--- src/Nav.js
+++ src/Nav.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import "./Nav.css";
+import './Nav.css';
 
 const Nav = props => {     // functional component- has no state, is controlling no part of the UI
     return (
         <div class="navbar">
-        <a href="#History">History</a>
-        <a href="#User">User</a>
+        <a href="#About">About</a>
         <a href="#Timer">Timer</a>
-
+        <a href="#History">History</a>
         </div>
 
     );
