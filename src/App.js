edffa21db1bd4942444013b85fc671c20f1f56f2
Mohammad Faraz <msf8tq@virginia.edu>
2020-05-20 14:03:17 -0400

    Added History.js file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import About from './About.js'
+import Nav from './Nav.js'
+import History from './History.js'
 
 class App extends Component {
   render() {
     return (
+<div>
+      <Nav/>
       <About />
+     <History />
+      </div>
     );
   }
 }
--- src/history.js
+++ src/history.js
@@ -1,57 +1,105 @@
-import Chart from 'chart.js';
-import './App.js';
-
-// data: [{
-//     x: "Jan 1 2020",
-//     y: "8:20 AM"
-// }, {
-//     x: "Jan 1 2020",
-//     y: "10:45 AM"
-// }, {
-//     x: "Jan 2 2020",
-//     y: "5:00 PM"
-// }]
-
-let data = data [{
-    x: "Jan 1 2020",
-    y: "8:20 AM"
-}, {
-    x: "Jan 1 2020",
-    y: "10:45 AM"
-}, {
-    x: "Jan 2 2020",
-    y: "5:00 PM"
-}]
+import React, { Component } from 'react';
+import {Bar, Charts, defaults} from 'react-chartjs-2';
 
 
-let historyChart = new Chart(ctx, {
-    type: 'bar',
-    data: data,
-    options: {
-        scales: {
-            xAxes: [{
-                stacked: true,
-                type: 'time',
-                time: {
-                    displayFormats: {
-                        day: 'MMM D YYYY' // Format example: Jan 1 2020
-                    }
-                }
-            }],
-            yAxes: [{
-                stacked:true,
-                time: {
-
-                    displayFormats: {
-                        minute: 'h:mm a' // Format example: 11:20 AM
-                    }
-
-                }
-            }]
-
-
+class History extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            // Not sure if this is the correct way to set up
+            // the state for 'time' and 'date' since we are
+            // obtaining these values from a Firebase dataset
+            chartData:{
+                date: "",
+                time: ""
+            }
         }
     }
-})
+
+    // Source: https://www.youtube.com/watch?v=p4XTMvagQ2Q
+    // This method shows the inital dataset (ex: a user will have a dataset with no values)
+    // componentDidMount() {
+    //     const rootRef = firebase.database().ref().child('react'); // Whatever is inside '.child()' argument is the root of the database
+    //     const timeRef = rootRef.child('time');
+        
+    //     // Note: the '.on' method allows us to synchronize data in real-time
+    //     timeRef.on('value', snap => {
+    //         this.setState({
+    //             time: snap.val()
+    //         });
+    //     });
+
+    //     const dateRef = rootRef.child('date');
+    //     dateRef.on('value', snap => {
+    //         this.setState({
+    //             date: snap.val()
+    //         });
+    //     });
+    // }
+
+    // // React life cycle hooks with firebase updates
+    // // This method shows the dataset after it's updated (ex: a user will have values in their dataset after using the timer)
+    // componentDidUpdate(prevProp, prevState) {
+    //     const rootRef = firebase.database().ref().child('react'); // Whatever is inside '.child()' argument is the root of the database
+    //     const timeRef = rootRef.child('time');
+        
+    //     // Note: the '.on' method allows us to synchronize data in real-time
+    //     if(prevState.time != this.state.time){
+    //         timeRef.on('value', snap => {
+    //             this.setState({
+    //                 time: snap.val()
+    //             });
+    //         });
+    //     }
+
+    //     if(prevState.date != this.state.date){
+    //         const dateRef = rootRef.child('date');
+    //         dateRef.on('value', snap => {
+    //             this.setState({
+    //                 date: snap.val()
+    //             });
+    //         });
+    //     }
+
+    // }
+
+    render(){
+        return (
+            <div classname='Chart'>
+            <Bar 
+                data={this.state.chartData}
+                width={50}
+                height={250}
+                options={{
+                    maintainAspectRatio: false,
+                    scales:{
+                        xAxes:[{
+                            stacked: true,
+                            type: 'time',
+                            time:{
+                                unit: 'week',
+                                displayFormats:{
+                                    week: 'll'
+                                }
+                            }
+                        }],
+                        yAxes: [{
+                            stacked: true,
+                            type: 'time',
+                            time:{
+                                displayFormats: {
+                                    minute: 'h:mm a'
+                                }
+                            }
+                        }]
+                    },
+                }}
+                />
+            </div>
+        )
+    }
+}
 
 
+
+export default History;
