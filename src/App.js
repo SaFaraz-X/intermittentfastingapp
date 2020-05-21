5c93ef9c43ebe2a352b57c41cd2ddefb281f5ab5
Mohammad Faraz <msf8tq@virginia.edu>
2020-05-21 11:13:51 -0400

    added history and clock js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import './Home.css';
-import Home from './Home.js'
-import Nav from './Nav.js'
-//import History from './history.js'
+import Home from './Home.js';
+import Nav from './Nav.js';
+import History from './History.js';
+import Clock from './Clock.js';
+
 
 class App extends Component {
   render() {
@@ -10,7 +12,8 @@ class App extends Component {
       <div>
         <Nav/>
         <Home />
-        {/* <History /> */}
+        <Clock />
+        <History />
       </div>
     );
   }
