4b16b4bdc34761271e18b10e22071d4ed4bba057
Mohammad Faraz <msf8tq@virginia.edu>
2020-05-21 15:44:01 -0400

    new updates

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import Home from './Home.js';
 import Nav from './Nav.js';
 import History from './History.js';
 import Clock from './Clock.js';
+import Timer from './Timer.js';
+import { Chart } from "react-google-charts";
+
+
+
+
 
 
 class App extends Component {
@@ -14,6 +20,10 @@ class App extends Component {
         <Home />
         <Clock />
         <History />
+        <Timer
+        githubURL="https://github.com/completejavascript/pomodoro-clock" 
+        defaultBreakLength='8' 
+        defaultSessionLength='16' />
       </div>
     );
   }
